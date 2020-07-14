0232d18d6f590e72fa26ca90a561f10ac5c0e088
Katayama-Daisuke <66638922+Katayama-Daisuke@users.noreply.github.com>
2020-07-14 10:10:24 +0900

    eqless

--- discordbot.py
+++ discordbot.py
@@ -5,21 +5,9 @@ from datetime import datetime, timedelta
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
-client = discord.Client()
 
 
 @bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-@client.event
 async def on_voice_state_update(member, before, after): 
     if member.guild.id == 732216126976819252 and (before.channel != after.channel):
         now = datetime.utcnow() + timedelta(hours=9)
@@ -32,6 +20,12 @@ async def on_voice_state_update(member, before, after):
             msg = f'{now:%m/%d-%H:%M} に {member.name} が {before.channel.name} から退出しました。'
             if before.channel.id == 732216127450513431:
                 await alert_channel.send(msg)
+
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
 
 
 bot.run(token)
