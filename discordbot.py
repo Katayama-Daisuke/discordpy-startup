d5816e50b7e22c6bd01e04e1a13764223a811c71
Katayama-Daisuke <66638922+Katayama-Daisuke@users.noreply.github.com>
2020-07-14 15:32:01 +0900

    eqless

--- discordbot.py
+++ discordbot.py
@@ -8,16 +8,16 @@ token = os.environ['DISCORD_BOT_TOKEN']
 
 @bot.event
 async def on_voice_state_update(member, before, after): 
-    if member.guild.id == 732216126976819252 and (before.channel != after.channel):
+    if member.guild.id == 311848727759421442 and (before.channel != after.channel):
         now = datetime.utcnow() + timedelta(hours=9)
-        alert_channel = bot.get_channel(732216127450513430)
+        alert_channel = bot.get_channel(732484072844034109)
         if before.channel is None: 
             msg = f'{now:%m/%d-%H:%M} に {member.name} が {after.channel.name} に参加しました。'
-            if after.channel.id == 732216127450513431:
+            if after.channel.id == 732484197255610368:
                 await alert_channel.send(msg)
         elif after.channel is None: 
             msg = f'{now:%m/%d-%H:%M} に {member.name} が {before.channel.name} から退出しました。'
-            if before.channel.id == 732216127450513431:
+            if before.channel.id == 732484197255610368:
                 await alert_channel.send(msg)
 
 @bot.command()
